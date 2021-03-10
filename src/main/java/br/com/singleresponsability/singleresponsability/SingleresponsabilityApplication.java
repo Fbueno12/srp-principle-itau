14f1cff346197b054c45da2828b82aeb0f6ae754
Felipe Bueno <felipebueno181@gmail.com>
2021-03-09 21:01:50 -0300

    Initial commit

---
+++ src/main/java/br/com/singleresponsability/singleresponsability/SingleresponsabilityApplication.java
@@ -0,0 +1,13 @@
+package br.com.singleresponsability.singleresponsability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleresponsabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleresponsabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/singleresponsability/singleresponsability/SingleresponsabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.singleresponsability.singleresponsability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleresponsabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
